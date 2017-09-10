31c6c54856d4f830f91b169084f8be7fa80a2db8
seansio1995 <seansio@163.com>
2017-09-10 14:26:03 -0400

    finish backend

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ def connect():
 
 
 def insert(product,price,year,name,phone,email,address):
-    conn=sqlite3.connect("products")
+    conn=sqlite3.connect("products.db")
     cur=conn.cursor()
     cur.execute("insert into products values (NULL,?,?,?,?,?,?,?)",(product,price,year,name,phone,email,address))
     conn.commit()
@@ -18,4 +18,34 @@ def insert(product,price,year,name,phone,email,address):
 
 
 def view():
-    
+    conn=sqlite3.connect("products.db")
+    cur=conn.cursor()
+    cur.execute("select * from products")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+
+def search(product="",price=0,year=0,name="",phone="",email="",address=""):
+    conn=sqlite3.connect("products.db")
+    cur=conn.cursor()
+    cur.execute("select * from products where product=? or price=? or year=? or name=? or phone=? or email=? or address=?",(product,price,year,name, phone, email, address))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("products.db")
+    cur=conn.cursor()
+    cur.execute("delete from where id=?",(id,))
+    conn.commit()
+    conn.close()
+
+
+
+def update(id,product,price,year,name,phone,email,address):
+    conn=sqlite3.connect("products.db")
+    cur=conn.cursor()
+    cur.execute("update products set product=?, price=?, year=?, name=?, phone=?,email=?, address=? where id=?",(product,price,year,name,phone,email,address,id))
+    conn.commit()
+    conn.close()
